f81af02468218f567c0dc60cdca7a2ad64568a18
KJHHHHHHH123 <kjhgfd6631@gmail.com>
2024-12-21 16:35:46 +0900

    20241221

--- Practice10_06/Practice10_06.cpp
+++ Practice10_06/Practice10_06.cpp
@@ -5,28 +5,32 @@ using namespace std;
 template <class T>
 T* remove(T src[], int sizeSrc, T minus[], int sizeMinus, int& retSize) {
 	retSize = sizeSrc;
-	cout << retSize;
-	for (int i = 0; i < sizeSrc; i++) {
+	T* newT = new T[retSize];
+
+	for (int i = 0; i < retSize; i++) {
+		newT[i] = src[i];
+	}
+
+	for (int i = 0; i < retSize; i++) {
 		for (int j = 0; j < sizeMinus; j++) {
-			if (src[i] == minus[j]) {
-				src[i] = src[i + 1];
-				sizeSrc--;
+			if (newT[i] == minus[j]) {
+				for (int z = i; z < retSize - 1; z++) {
+					newT[z] = newT[z + 1];
+				}
+				retSize--;
 			}
 		}
 	}
-	retSize = sizeSrc;
-	cout << retSize;
-	T* newT = new T[retSize];
 
 	return newT;
 }
 
 int main() {
-	int x[] = { 1,2,3,4,5 };
-	int y[] = { 2,3 };
+	int x[] = { 1,2,3,4,5,6,7,8 };
+	int y[] = { 2,3,6,7 };
 	int retSize = 0;
 
-	int* newT = remove(x, 5, y, 2, retSize);
+	int* newT = remove(x, 8, y, 4, retSize);
 
 	for (int i = 0; i < retSize; i++) {
 		cout << newT[i] << " ";
